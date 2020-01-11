c077085faad31a8d3697af4d4ef6ef25cdab9723
designermanjeets@gmail.com <designermanjeets@gmail.com>
2020-01-11 18:16:53 +0530

    header2

--- apps/awsmscreativepixel/src/app/app.component.ts
+++ apps/awsmscreativepixel/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, HostBinding } from '@angular/core';
 import { fade } from './animation/animation';
 
+export interface Menu {
+  name: string;
+  route?: string;
+}
+
 @Component({
   selector: 'awsworkspace-root',
   templateUrl: './app.component.html',
@@ -11,9 +16,13 @@ import { fade } from './animation/animation';
 export class AppComponent {
   title = 'awsmscreativepixel';
   loginOutput: any;
-  state = 'inactive';
-  times = 5;
-  counter = 1;
+  menuItems :Menu[] = [
+    { name: 'Home', route: 'home' },
+    { name: 'What we do', route: 'whatwedo' },
+    { name: 'Who we are', route: 'whoweare' },
+    { name: 'Get in touch', route: 'getintouch' },
+    { name: 'Login/Signup' },
+  ]
 
   constructor(){
 
@@ -27,13 +36,8 @@ export class AppComponent {
     console.log($event);
   }
 
-  onDone($event) {
-    // call this function at the end of the previous animation.
-    // run it as many time as defined
-    if (this.counter) { // < this.times
-      this.state = this.state === 'active' ? 'inactive' : 'active';
-      this.counter++;
-    }
+  getClickedItem($event){
+    console.log($event);
   }
 
 }
--- libs/awsheader/src/lib/header/header.component.ts
+++ libs/awsheader/src/lib/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, AfterViewInit } from '@angular/core';
+
+export interface Menu {
+  name: string;
+  route?: string;
+}
 
 @Component({
   selector: 'awsworkspace-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  @Input() menuItems: Menu[];
+  @Output() clickedItem = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  ngAfterViewInit(): void {
+    console.log(this.menuItems);    
+  }
+
+  onItemClick(event){
+    this.clickedItem.emit(event);
   }
 
 }
